95def603825a15ddfb488c895946af601ea1047e
farisrizqi <121332534+farisrizqi@users.noreply.github.com>
2023-07-16 14:08:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -33,8 +33,8 @@ void Main :: setValue() {
     cin.ignore();
     for (int i = 0; i < jml; i++) {
         cout << "Data ke-" << i + 1 << endl;
-        cout << "Nama " << " : "; getline(cin, arr[i].namaMahasiswa);
-        cout << "NIM " << "  : "; getline(cin, arr[i].nim);
+        cout << "Nama Lengkap\t: "; getline(cin, arr[i].namaMahasiswa);
+        cout << "NIM\t\t: "; getline(cin, arr[i].nim);
         cout << endl;
         arr[data.jmlMahasiwa].saveNama = arr[i].namaMahasiswa; 
         arr[data.jmlMahasiwa].saveNim = arr[i].nim; 
@@ -45,19 +45,23 @@ void Main :: setValue() {
 
 void Main :: getName (int jml) {
     if (data.jmlMahasiwa != 0) {
+        cout << "========= Data Mahasiswa =========\n";
         for (int i = 0; i < jml; i++) {
-            cout << i+1 << ". " <<  arr[i].saveNama << "/" << arr[i].saveNim << endl;
+            cout << i+1 << ". Nama Lengkap\t: " <<  arr[i].saveNama << "\n   NIM\t\t: " << arr[i].saveNim << "\n   Nilai\t: " << endl << endl;
         }
+        cout << "==================================\n";
     } else {
         cout << endl << "- Tidak Ada Data Mahasiswa -" << endl;;
     }
+    system("pause");
+    system("cls");
     setSystem();
-}
+} 
 /*END--INPUT DATA MAHASISWA*/
 
 void Main :: setSystem () {
     int pil;
-    cout << endl << "===========================" << endl;
+    cout << endl << "============================" << endl;
     cout << "1. Tambah Data Mahasiswa\n2. Lihat Data Mahasiwa\n3. Input Nilai\nPilihan : "; cin >> pil;
     if (pil == 1) {
         system("cls");
